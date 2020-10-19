32cb991ec4318ce839013575da78324da31effd2
Joshua Archibald <jarchibald121@gmail.com>
2020-10-18 21:28:17 -0400

    Did this actually fix it?

--- exercises/7/ozai/__init__.py
+++ exercises/7/ozai/__init__.py
@@ -38,23 +38,18 @@ def update_works():
     db._close_session()
 
     res = check50.run("sqlite3 cabbages.db < ozai.sql").stdout()
-
-    #res = check50.run('sqlite3 cabbages.db "SELECT id FROM invoices WHERE customer_id={}"'.format(ozai_id[0]["id"])).stdout()
     res2 = check50.run('sqlite3 cabbages.db "SELECT id FROM invoices WHERE customer_id={}"'.format(ozai_id[0]["id"])).stdout()
-    raise check50.Mismatch(res, res2)
-
-    #n = db.execute("SELECT COUNT(id) FROM invoices WHERE customer_id=?", ozai_id[0]["id"])
-
-    #if n[0]["COUNT(id)"] > 0:
-    #    raise check50.Mismatch("UPDATE query fails to erase all references to Ozai in invoices", n)
+    
+    if res2 != "":
+        raise check50.Mismatch("", res2)
 
 
 @check50.check(update_works)
 def delete_works():
     """DELETE query erases Ozai from customers table"""
     
-    db = SQL("sqlite:///cabbages.db")
-    n = db.execute("SELECT COUNT(id) FROM customers WHERE name=?", "Ozai")
-    if n[0]["COUNT(id)"] > 0:
-        raise check50.Failure("DELETE query fails to erase Ozai from customers table")
+    res = check50.run('sqlite3 cabbages.db "SELECT id FROM customers WHERE name=Ozai"').stdout()
+    
+    if res != "":
+        raise check50.Mismatch("", res)
  
